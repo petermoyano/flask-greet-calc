5f8a03eccc389003d437d19a0ee2d4a32e53f53e
Pedro Moyano <pedromoyano454@gmail.com>
2021-09-11 13:22:23 -0300

    All tests passed

---
+++ greet/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/welcome')
+def say_welcome():
+    return "welcome"
+
+@app.route('/welcome/home')
+def say_welcome():
+    return "welcome home"
+
+@app.route('/welcome/back')
+def say_welcome():
+    return "welcome back"
